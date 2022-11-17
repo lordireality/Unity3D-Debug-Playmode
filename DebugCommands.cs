90e84a41cc13df36bfbe04ba7871b417b159ed82
German <68060177+lordireality@users.noreply.github.com>
2022-11-17 10:14:36 +0300

    uploaded

---
+++ DebugCommands.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugCommands : MonoBehaviour
+{
+    public InputField cmdPromt;
+
+    public void SendCommand()
+    {
+        Debug.Log(">>" + cmdPromt.text);
+        InterpritateCMD(cmdPromt.text);
+        cmdPromt.text = "";
+    }
+    public void InterpritateCMD(string rawCommand)
+    {
+        rawCommand = rawCommand.ToLower();
+        string[] splitCommand = rawCommand.Split(' ');
+        switch (splitCommand[0])
+        {
+            case "help": HelpCMD(); break;
+            case "time": TimeCMD(); break;
+            case "setquality": SetQualityCMD(splitCommand[1]); break;
+            case "showtriggers": ShowTriggersZonesCMD(splitCommand[1]); break;
+            default: NotFoundCMD(); break;
+        }
+    }
+    private void NotFoundCMD()
+    {
+        Debug.Log("<color=red>Console</color>|Command not found");
+    }
+    private void HelpCMD()
+    {
+        Debug.Log("<color=red>Console</color>|<color=yellow>help</color> - for all list of commands");
+        Debug.Log("<color=red>Console</color>|<color=yellow>time</color> - show current PC time");
+        Debug.Log("<color=red>Console</color>|<color=yellow>setquality + 1args</color> - set global graphics settings to specific level");
+        Debug.Log("<color=red>Console</color>|<color=yellow>showtriggers + 1args</color> - show and hide triggers zones");
+    }
+    private void TimeCMD()
+    {
+        Debug.Log("<color=red>Console</color>|Now is: " + DateTime.Now);
+    }
+    private void SetQualityCMD(string arg1)
+    {
+        bool isNum = Int32.TryParse(arg1, out int index);
+        if (isNum) { QualitySettings.SetQualityLevel(index); Debug.Log("<color=red>Console</color>|Quality level set to: " + QualitySettings.GetQualityLevel()); }
+        else { Debug.Log("<color=red>Console</color>|Args must be numeric!"); }
+    }
+    private void ShowTriggersZonesCMD(string arg1)
+    {
+        if(arg1 == "true")
+        {
+            GameObject[] triggers = GameObject.FindGameObjectsWithTag("Trigger");
+            foreach (GameObject go in triggers)
+            {
+                go.GetComponent<MeshRenderer>().enabled = true;
+            }
+            Debug.Log("<color=red>Console</color>|Triggers zones shown");
+        }
+        else if (arg1 == "false")
+        {
+            GameObject[] triggers = GameObject.FindGameObjectsWithTag("Trigger");
+            foreach (GameObject go in triggers)
+            {
+                go.GetComponent<MeshRenderer>().enabled = false;
+            }
+            Debug.Log("<color=red>Console</color>|Triggers zones hidden");
+        } else { Debug.Log("<color=red>Console</color>|Args must be bool! true/false"); }
+    }
+    private void ShowOtherPlayers(string arg1)
+    {
+
+    }
+}
---
+++ DebugConsole.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class DebugConsole : MonoBehaviour
+{
+    private string output;
+    private string stack;
+    public Text consoleOutputUI;
+    public GameObject consoleUIObj;
+
+    private void Start()
+    {
+        CheckForDebugFile();
+    }
+    void CheckForDebugFile()
+    {
+        if (!Directory.Exists(@"game-data")) { Directory.CreateDirectory(@"game-data"); }
+        if (!File.Exists(@"game-data/debugLog.dgt")) { File.Create(@"game-data/debugLog.dgt"); }
+        
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.BackQuote))
+        {
+            if (!consoleUIObj.activeSelf) { consoleUIObj.SetActive(true); } else
+            if (consoleUIObj.activeSelf) { consoleUIObj.SetActive(false); }
+        }
+
+        Application.RegisterLogCallback(HandleLog);
+    }
+
+    private void OnEnable()
+    {
+        Application.RegisterLogCallback(HandleLog);
+    }
+
+    private void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        output = type + ": " + logString + "\n" + output;
+        File.AppendAllText(@"game-data/debugLog.dgt", type + ": " + logString + "\n");
+        stack += stackTrace;
+        consoleOutputUI.text = output;
+    }
+}
